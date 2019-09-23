92dc1a3c371abab1274654d783ce30ff244ba9f6
Trung Ngo <ndtrung4419@gmail.com>
2019-09-23 07:58:40 +0300

    Delete the view folder, moving the NodeJS
    Co-authored-by: Nga Nguyen <nganguyen.ny@gmail.com>

--- view/app/controllers/eshop_controller.rb
+++
@@ -1,9 +0,0 @@
-class EshopController < ApplicationController
-  def index
-    # render :text => 'hahaha'
-    # return 'Hello world!'
-    # @data = File.read("/.")
-    # render :json => data,
-    render :file => 'public/europe.json', :content_type => 'application/json', :layout => false
-  end
-end
--- view/test/controllers/eshop_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class EshopControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get eshop_index_url
-    assert_response :success
-  end
-
-end
